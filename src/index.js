f703c417192ea18517bd7f0297324838b8d3b773
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2023-01-19 21:28:53 +0530

    code updated

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.json())
 mongoose.set('strictQuery', false)
 mongoose.connect("mongodb+srv://gaurav:Grv20072000@cluster0.3fqqw8s.mongodb.net/gaurav")
+
     .then(() => console.log("Mongodb is connected"))
     .catch(err => console.log(err))
 
--- src/route/route.js
+++ src/route/route.js
@@ -3,12 +3,11 @@ const router = express.Router()
 const urlController = require('../controller/urlController')
 
 router.post('/url/shorten', urlController.shortUrl)
-router.get("/:urlCode", urlController.geturl)
-
-router.get('/:urlCode', urlController.geturl)
+router.get("/:urlCode", urlController.getUrl)
 
 router.all("/*", function (req, res) {
-    res.status(400).send({ status: false, message: "invalid https request" })
+    res.status(404).send({ status: false, message: "invalid https request" })
 })
 
 module.exports = router
+
